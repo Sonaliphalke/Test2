db3966f4abc6801c12c8ffeb0a0579b2d16544dc
sonal <sonal@LAPTOP-FS2B3II7>
2022-10-12 19:29:08 +0530

    First commit

---
+++ src/GitHub/Test2.java
@@ -0,0 +1,40 @@
+package GitHub;
+
+public class Test2 {
+
+	
+	public static void main(String[] args) 
+	{
+	
+		String org="sonali";
+		String rev="";
+		
+		
+		for(int i=org.length()-1; i>=0; i-- )
+		{
+			
+			         rev=rev+org.charAt(i);
+			
+		}
+		
+		     System.out.println(rev);
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
